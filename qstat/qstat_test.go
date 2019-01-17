6f656c5c4c32d332e8db40e5c6486bfc6b5c2b29
Lei Tian <taylor840326@gmail.com>
2019-01-17 17:56:14 +0800

    修改测试函数

--- qstat/qstat_test.go
+++ qstat/qstat_test.go
@@ -7,6 +7,25 @@ import (
 	"github.com/taylor840326/go_pbspro/utils"
 )
 
+func TestServerLists(t *testing.T) {
+	handle, err := utils.Pbs_connect("172.18.7.10")
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err = utils.Pbs_disconnect(handle)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	sl := Pbs_get_server_list()
+	//Print Server State Informations.
+	fmt.Println(sl)
+
+}
+
 func TestServerStat(t *testing.T) {
 	handle, err := utils.Pbs_connect("172.18.7.10")
 	if err != nil {
