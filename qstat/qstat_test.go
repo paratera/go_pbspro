72a396b4217012e2e5aca7d83ba6ba3a40987457
Lei Tian <taylor840326@gmail.com>
2019-01-17 16:24:20 +0800

    在qstat目录下添加测试文件

--- qstat/qstat_test.go
+++ qstat/qstat_test.go
@@ -1,35 +1,31 @@
-package main
+package qstat
 
 import (
 	"fmt"
-	"log"
+	"testing"
 
-	"github.com/taylor840326/go_pbspro/qstat"
 	"github.com/taylor840326/go_pbspro/utils"
 )
 
-func main() {
+func TestQueryJobStat(t *testing.T) {
 	handle, err := utils.Pbs_connect("172.18.7.10")
 	if err != nil {
-		log.Fatal("Couldn't connect to server: %s", err)
+		t.Error(err)
 	}
 
 	defer func() {
 		err = utils.Pbs_disconnect(handle)
 		if err != nil {
-			log.Fatal("Disconnect failed: %s\n", err)
+			t.Error(err)
 		}
 	}()
 
-	/*
-	   _, err = pbs.Pbs_submit(handle, nil, "test.sh", "","")
-	*/
-	bs, err := qstat.Pbs_statjob(handle, "1045", nil, "")
+	bs, err := Pbs_statjob(handle, "1045", nil, "")
 	if err != nil {
-		log.Fatal("Job submission failed: %s\n", err)
+		t.Error(err)
 	}
 
+	//Print Job State Informations.
 	fmt.Println(bs)
-	//fmt.Fprintf(os.Stdout,"bs.Name=%s,bs.Text=%s",bs.Name,bs.Text)
 
 }
