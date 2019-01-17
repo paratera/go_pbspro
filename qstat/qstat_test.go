00d2e603d839b7a87d4e100fae0f0383f1d384d1
Lei Tian <taylor840326@gmail.com>
2019-01-17 17:52:35 +0800

    修改测试函数

--- qstat/qstat_test.go
+++ qstat/qstat_test.go
@@ -27,12 +27,42 @@ func TestQueryJobStat(t *testing.T) {
 	//Print Server State Informations.
 	fmt.Println(bs)
 
+}
+
+func TestQueryJobStat(t *testing.T) {
+	handle, err := utils.Pbs_connect("172.18.7.10")
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err = utils.Pbs_disconnect(handle)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
 	bs, err = Pbs_statque(handle, "workq", nil, "")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	//Print Queue State Informations.
 	fmt.Println(bs)
+
+}
+
+func TestQueryJobStat(t *testing.T) {
+	handle, err := utils.Pbs_connect("172.18.7.10")
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err = utils.Pbs_disconnect(handle)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
 
 	bs, err = Pbs_statjob(handle, "1045", nil, "")
 	if err != nil {
